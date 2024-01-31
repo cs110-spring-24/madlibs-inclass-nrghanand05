86ec3f5a275c64ba690f2c6e45ad78e16f2648a5
nrghanand05 <ghannadn5@gmail.com>
2024-01-31 00:47:07 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,18 @@
 # TODO Create your inputs, variables, and add your story here!
+noun = input("Give me a noun: ")
+adjective = input("Give me an adjective: ")
+activeverb = input ("Give me an active verb: ")
+print (f"The {adjective} {noun} was {activeverb}.")
+
+noun2= input ("Give me a different noun: ")
+verb2= input ("Give me a verb: ")
+place = input ("Give me a place: ")
+noun3 = input ("Give me another noun: ")
+activeverb2 = input ("Give me an another active verb: ")
+pluralnoun = input ("Give me a plural noun: ")
+animal= input ("Give me an animal: ")
+verb3= input ("Give me another verb: ")
+place2 = input ("Give me another place: ")
+adjective2 = input ("Give me another adjective: ")
+livingthing = input ("Give me a living thing: ")
+print (f"The {adjective} {noun} was {activeverb}. In the morning, {noun2} decided to {verb2} across the {place} to search for a new {noun3} while {activeverb2} through the {pluralnoun}. The {noun} and {animal} decided to {verb3} through the {place2} together searching for a {adjective2} {livingthing}.")
